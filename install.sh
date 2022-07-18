62703a0712c94fb2ec0fba68854bd21eb8aeb184
RoboChimera <83529181+RoboChimera@users.noreply.github.com>
2022-07-18 16:22:47 +0200

    Fixed Install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ echo "THIS REQUIRES ROOT!" &&
 pkg install electron19 www/npm node16 &&
 rm -rvf /usr/local/share/chimeracord &&
 cp -rvf chimeracord /usr/local/share/chimeracord &&
-rm -v /usr/local/bin/chimeracord &&
 cp -vf chimeracord.in /usr/local/bin/chimeracord && 
 chmod +x /usr/local/bin/chimeracord &&
-rm -v /usr/local/share/applications/chimeracord.desktop &&
 cp -vf chimeracord.desktop.in /usr/local/share/applications/chimeracord.desktop
