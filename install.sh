dfe9371c1719ea73dcd616a070f89e260ff9a941
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2022-07-26 16:28:46 +0200

    npm replaces electron19 from pkg, so i put pkg at the lsat part, hopefully it works

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 echo "ChimeraCord Installer" &&
 echo "THIS REQUIRES ROOT!" &&
-if [ -f /usr/sbin/pkg ];  then
-	echo "FreeBSD package manager detected, dependencies will automatically install :)" &&
-	pkg install electron19 www/npm node16
-fi
 
 echo "Copying Files..." &&
 rm -rvf /usr/local/share/chimeracord &&
@@ -17,4 +13,8 @@ fi
 
 echo "Installing NPM packages inside of /usr/local/share/chimeracord" &&
 cd /usr/local/share/chimeracord &&
-npm install electron-context-menu
+npm install -gf electron-context-menu &&
+if [ -f /usr/sbin/pkg ];  then
+	echo "FreeBSD package manager detected, dependencies will automatically install :)" &&
+	pkg install electron19 www/npm node16
+fi
