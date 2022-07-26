545c1c9a5e98b4db509ee0ddccf52312d5053242
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2022-07-26 16:04:43 +0200

    Install.sh will now check if you have FreeBSD's package manager

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 echo "ChimeraCord Installer" &&
 echo "THIS REQUIRES ROOT!" &&
-pkg install electron19 www/npm node16 &&
+if [ -f /usr/local/bin/pkg ];  then
+	echo "FreeBSD package manager detected, dependencies will automatically install :)" &&
+	pkg install electron19 www/npm node16
+fi
 
 echo "Copying Files..." &&
 rm -rvf /usr/local/share/chimeracord &&
 cp -rvf chimeracord /usr/local/share/chimeracord &&
 cp -vf chimeracord.in /usr/local/bin/chimeracord && 
 chmod +x /usr/local/bin/chimeracord &&
-cp -vf chimeracord.desktop.in /usr/local/share/applications/chimeracord.desktop &&
+if [ -f /usr/local/share/applications ]; then
+	cp -vf chimeracord.desktop.in /usr/local/share/applications/chimeracord.desktop
+fi
 
 echo "Installing NPM packages inside of /usr/local/share/chimeracord" &&
 npm install -gf electron-context-menu
