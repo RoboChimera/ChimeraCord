2661b278bd687072e91d245da104ced135cb46d1
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2022-07-26 16:46:18 +0200

    o

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ fi
 echo "Installing NPM packages inside of /usr/local/share/chimeracord" &&
 cd /usr/local/share/chimeracord &&
 npm install -f electron-context-menu &&
+rm -rf /usr/local/share/chimeracord/nodes_modules/electron &&
 if [ -f /usr/sbin/pkg ];  then
 	echo "FreeBSD package manager detected, dependencies will automatically install :)" &&
 	pkg install electron19 www/npm node16
