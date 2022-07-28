79639e3b5940441d7fa304bfb040ecb483227086
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2022-07-28 16:32:54 +0200

    Added ContextMenu support for freebsd using softlinks

--- install.sh
+++ install.sh
@@ -14,8 +14,7 @@ fi
 echo "Installing NPM packages inside of /usr/local/share/chimeracord" &&
 cd /usr/local/share/chimeracord &&
 npm install -gf electron-context-menu --save-dev &&
-ln -sFf /usr/local/lib/node_modules /usr/local/share/node_modules
-rm -rf /usr/local/share/chimeracord/nodes_modules/electron &&
+ln -sFf /usr/local/lib/node_modules /usr/local/share/chimeracord/node_modules &&
 if [ -f /usr/sbin/pkg ];  then
 	echo "FreeBSD package manager detected, dependencies will automatically install :)" &&
 	pkg install electron19 www/npm node16
